54510169ce5251247a9f933fccbddb47b1f727b2
CCapslock <53517856+CCapslock@users.noreply.github.com>
2020-11-12 22:45:34 +0300

    Balance controller add

---
+++ Assets/BalanceModel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+namespace Vagonetka
+{
+	public class BalanceModel : MonoBehaviour
+	{
+		public BalancePreset BalancePreset;
+
+		public float CountSpeed(int CurrentLVL)
+		{
+			return 2;
+		}
+	}
+}
---
+++ Assets/Scripts/ForScriptableObjects/FewLVLPreset.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+
+namespace Vagonetka
+{
+    [Serializable]
+    public class FewLVLPreset
+    {
+        public int MinLvl;
+        public int MaxLvl;
+        public SingleLvl LevelSettings;
+    }
+}
