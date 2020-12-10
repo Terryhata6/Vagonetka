a1039d7bb74883dad62a1dd1fc0752507300c257
DronCorleone <48313301+DronCorleone@users.noreply.github.com>
2020-12-11 01:42:42 +0300

    Doors + animation

---
+++ Assets/Scripts/Triggers/DoorOpenTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+namespace Vagonetka
+{
+    public class DoorOpenTrigger : MonoBehaviour
+    {
+        private string _open = "Open";
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<PlayerModel>())
+            {
+                transform.parent.GetComponentInChildren<Animator>().SetBool(_open, true);
+            }
+        }
+    }
+}
